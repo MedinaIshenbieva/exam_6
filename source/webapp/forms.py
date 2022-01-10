dd82186d1a0db7c096f0dd95708714aaa54940a5
MedinaIshenbieva <medinanurlanova7@gmail.com>
2022-01-11 00:01:55 +0300

    добавила forms.py

---
+++ source/webapp/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.forms import widgets
+
+from webapp.models import STATUS_CHOICES
+
+
+class GuestBookForm(forms.Form):
+    author = forms.CharField(max_length=200, required=True, label="Автор")
+    email = forms.EmailField(max_length=200, required=True, label="Имейл")
+    text = forms.TextField(max_length=2000, required=True, label="Текст записи")
+    status = forms.CharField(choices=STATUS_CHOICES)
